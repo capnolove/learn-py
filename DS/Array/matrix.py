d2c1d60fc7303b92110e6d87f06d7b0247e8b916
capnolove <insocdola@gmail.com>
2024-05-18 02:40:20 +0800

    Update matrix.py

--- DS/Array/matrix.py
+++ DS/Array/matrix.py
@@ -1,6 +1,8 @@
 from numpy import *
 
+# shaping this 1-d array into 2-d array with 3 rows and 3 cols
 a=(43,33,31,314,42,42,45,13,10) 
 m=reshape(a,(3,3))
-print(m)
+print(m[2][2]) # print data at index 2,2
+
 
