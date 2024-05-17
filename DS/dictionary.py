4c934e8779daa6afb5286a814d1f1e4e94544ac1
capnolove <insocdola@gmail.com>
2024-05-17 21:24:35 +0800

    Finish dictionary and add theory for hash table

--- DS/dictionary.py
+++ DS/dictionary.py
@@ -3,8 +3,16 @@
 # empty dictionary is written by {}
 # keys must be a data type such as strings, numbers or tuples
 
+# declare a dictionary
 dict={"Name":"Laptop", 'Color': ["red","blue"]}
 
 # accessing dictionary items
 print (dict["Color"])
 print (dict["Name"])
+
+dict["Color"]="red", "blue", "purple" # update existing entry
+dict["Brand"]="Dell"                  # add new entry
+
+del dict['Name'] # remove entry with key 'Name'
+dict.clear()     # remove all entries in dict
+del dict         # delete entire dictionary
